e274516f2ad322625ab7802e3e1b80993365b02b
mostafa1411 <mostafa.abdullah1411@gmail.com>
2022-10-12 23:51:38 +0200

    Make creation time field read-only in django admin

--- musicplatform/albums/admin.py
+++ musicplatform/albums/admin.py
@@ -4,3 +4,7 @@ from .models import Album
 # Register your models here.
 
 admin.site.register(Album)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_time', )
--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -7,7 +7,8 @@ from artists.models import Artist
 
 class Album(models.Model):
     name = models.CharField(max_length=50, default=u'New Album')
-    creation_time = models.DateTimeField(default=timezone.now)
+    creation_time = models.DateTimeField(default=timezone.now, editable=False)
     release_time = models.DateTimeField(blank=False)
     cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, default='')
+    approved = models.BooleanField(default=False)
