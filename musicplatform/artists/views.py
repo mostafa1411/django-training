e67ce53975124151071757bef3ac6c09578bcdb0
mostafa1411 <mostafa.abdullah1411@gmail.com>
2022-10-19 23:25:15 +0200

    View list of artists with their albums

--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .forms import ArtistFrom
+from .models import Artist, Album
 
 
 # Create your views here.
+
+def index(request):
+    context = {'artists': Artist.objects.all()}
+    return render(request, 'artists/index.html', context)
+
 
 def add_artist(request):
     if request.method == "POST":
