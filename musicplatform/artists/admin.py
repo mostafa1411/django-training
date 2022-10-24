98aea5e32916e784f37cbd3221125cb9615a821d
mostafa1411 <mostafa.abdullah1411@gmail.com>
2022-10-24 03:46:51 +0200

    Remove the album inline from the artist admin form

--- musicplatform/artists/admin.py
+++ musicplatform/artists/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .models import Artist, Album
-
-
-class AlbumInline(admin.StackedInline):
-    model = Album
-    extra = 0
+from .models import Artist
 
 
 class ArtistAdmin(admin.ModelAdmin):
     model = Artist
     list_display = ('stage_name', 'approved_albums_count')
-    inlines = [AlbumInline]
 
 
 # Register your models here.
