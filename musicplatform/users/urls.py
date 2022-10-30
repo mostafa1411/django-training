65bb39cd3ff0c9647546343da5aa0e7fc4f762e0
mostafa1411 <mostafa.abdullah1411@gmail.com>
2022-10-30 11:06:50 +0200

    Create view set for User

--- musicplatform/users/urls.py
+++ musicplatform/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.UserView.as_view(), name='user')
+    path('<int:pk>/', views.UserViewSet.as_view({
+        'get': 'retrieve', 'put': 'update', 'patch': 'partial_update'
+    }), name='user')
 ]
--- musicplatform/users/views.py
+++ musicplatform/users/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status, permissions
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 from .models import User
 from .serializers import UserSerializer
 
@@ -7,22 +6,7 @@ from .serializers import UserSerializer
 # Create your views here.
 
 
-class UserView(generics.GenericAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-
-    def get(self, request, pk):
-        users = list(User.objects.filter(id=pk))
-
-        if not users:
-            return Response("User is not found", status=status.HTTP_404_NOT_FOUND)
-
-        user = users[0]
-
-        return Response({
-            "user": {
-                "id": user.pk,
-                "username": user.username,
-                "email": user.email,
-                "bio": user.bio
-            }
-        }, status=status.HTTP_200_OK)
+    permission_classes = {permissions.IsAuthenticatedOrReadOnly}
+    queryset = User.objects.all()
